559e0a9df7b1742dba0b5f2f80495b7b7f4a594f
Alan Hernandez <alan_herlo@hotmail.com>
2023-09-07 10:09:29 -0600

    UC#10 Creating regex expression

--- src/main/java/com/password/validation/softserve/SoftserveApplication.java
+++ src/main/java/com/password/validation/softserve/SoftserveApplication.java
@@ -3,6 +3,8 @@ package com.password.validation.softserve;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.regex.Pattern;
+
 @SpringBootApplication
 public class SoftserveApplication {
 
@@ -10,4 +12,11 @@ public class SoftserveApplication {
 		SpringApplication.run(SoftserveApplication.class, args);
 	}
 
+	public static boolean validatePassword(int length, String password) {
+		String passwordPattern
+				= "^(?=.*[0-9])(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[!\\\"#$%&'()*+,-\\.\\/:;<=>?@\\[\\\\\\]^_`{|}~])(?!.*?[\\t\\s\\r]).{" + length + "}$";
+		Pattern pattern = Pattern.compile(passwordPattern);
+
+		return pattern.matcher(password).matches();
+	}
 }
