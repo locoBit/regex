12cd27fa566150c79b83993d8e9811015927946d
Alan Hernandez <alan_herlo@hotmail.com>
2023-09-07 08:14:51 -0600

    UC#10 Start

---
+++ src/main/java/com/password/validation/softserve/SoftserveApplication.java
@@ -0,0 +1,13 @@
+package com.password.validation.softserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/password/validation/softserve/SoftserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.password.validation.softserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
